25940b3d48c122948976b751fb9175c741cd7359
S A HARIPRASAD <myharicode@gmail.com>
2021-11-02 22:46:58 +0530

    working with operational arguments

--- graph-ql/src/index.js
+++ graph-ql/src/index.js
@@ -7,32 +7,64 @@ import { GraphQLServer } from 'graphql-yoga';
 //Type definitions schema (schema)
 const typeDefs = `
     type Query{
-        title:String!,
-        price:Float!,
-        releaseYear:Int,
-        rating:Float,
-        inStock:Boolean!
+        greeting(name: String,position:String):String!
+        add(a: Float!,b: Float!):Float!
+        me:User!
+        post:Post!
+    }
+
+    type User{
+        id: ID!,
+        name:String!,
+        email:String!,
+        age:Int
+    }
+
+    type Post{
+        id:ID!,
+        body:String!,
+        published:String!,
     }
 `
 
 //Resolvers
 const resolvers = {
     Query: {
-        title() {
-            return "Maths Book"
+        add(parent, args, ctx, info) {
+            if(args.a && args.b){
+                return args.a + args.b;
+            }
+            else{
+                return 0;
+            }
         },
-        price() {
-            return 22.5
+        greeting(parent, args, ctx, info) {
+            //console.log(parent);
+            console.log(args);
+            //console.log(ctx);
+            //console.log(info);
+            if (args.name && args.position) {
+                return `Hello , ${args.name} you are my favorite ${args.position}`
+            } else {
+                return 'Hello!'
+            }
         },
-        releaseYear() {
-            return 2021
+        me() {
+            return {
+                id: '3257',
+                name: 'mike',
+                email: "mike@gmail.com",
+                age:28
+            }
         },
-        rating() {
-            return 4.2
-        },
-        inStock() {
-            return 3.5;
+        post() {
+            return {
+                id: '123',
+                body: "myFirstpost",
+                published:"29/07/21"
+            }
         }
+
 
     }
 }
