b3fb08ecdef8f904447bb9e6ba1fc683e7c8fa9c
S A HARIPRASAD <myharicode@gmail.com>
2021-11-06 22:43:55 +0530

    data validation and sanitisation for users model

--- Task-manager/src/db/mongoose.js
+++ Task-manager/src/db/mongoose.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
+
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -7,9 +9,39 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
 const User = mongoose.model("User", {
   name: {
     type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.includes("password")) {
+        throw new Error("password should not contain password as string");
+      }
+    },
   },
   age: {
     type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("age must be a positive number");
+      }
+    },
   },
 });
 
@@ -23,27 +55,30 @@ const Tasks = mongoose.model("Tasks", {
 });
 
 const me = new User({
-  name: "hariprasad",
-  age: 27,
+  name: "master",
+  email: "master@gmail.com",
+  password: "helloworld",
+  age: 20,
 });
 
-const task = new Tasks({
-  description: "Complete node js course",
-  completed: false,
-});
+// const task = new Tasks({
+//   description: "Complete node js course",
+//   completed: false,
+// });
 
-task
-  .save()
-  .then(() => {
-    console.log(task);
-  })
-  .catch((error) => {
-    console.log("Error", error);
-  });
-// me.save()
+// task
+//   .save()
 //   .then(() => {
-//     console.log(me);
+//     console.log(task);
 //   })
 //   .catch((error) => {
-//     console.log("Error!", error);
+//     console.log("Error", error);
 //   });
+
+me.save()
+  .then(() => {
+    console.log(me);
+  })
+  .catch((error) => {
+    console.log("Error!", error);
+  });
