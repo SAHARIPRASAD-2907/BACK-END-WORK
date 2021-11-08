ba25cfed5344668efc1887d5de59ed36e88e0562
S A HARIPRASAD <myharicode@gmail.com>
2021-11-08 16:13:49 +0530

    setting up get request for users and tasks

--- Task-manager/src/index.js
+++ Task-manager/src/index.js
@@ -20,11 +20,12 @@ app.get("/", (req, res) => {
 
 // User endpoints
 //user creation
-app.post("/user", (req, res) => {
+app.post("/users", (req, res) => {
   const user = new User(req.body);
   user
     .save()
     .then(() => {
+      res.send(201);
       res.send(user);
     })
     .catch((e) => {
@@ -32,17 +33,61 @@ app.post("/user", (req, res) => {
       res.send(e);
     });
 });
+//read users
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch(() => {
+      res.status(500).send("Error in accessing data");
+    });
+});
+//read single user
+app.get("/users/:id", (req, res) => {
+  User.find({ id: req.params.id })
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((e) => {
+      res.status(500).send(e);
+    });
+});
 
 //Tasks endpoint
-app.post("/task", (req, res) => {
+// Task creation
+app.post("/tasks", (req, res) => {
   const task = new Task(req.body);
   task
     .save()
     .then(() => {
+      res.send(201);
       res.send(task);
     })
     .catch((e) => {
       res.status(400);
       res.send(e);
+    });
+});
+
+//read tasks
+
+app.get("/tasks", (req, res) => {
+  Task.find({})
+    .then((task) => {
+      res.send(task);
+    })
+    .catch((e) => {
+      res.status(500).send(e);
+    });
+});
+
+app.get("/tasks/:id", (req, res) => {
+  Task.find({ id: req.params.id })
+    .then((task) => {
+      res.send(task);
+    })
+    .catch((e) => {
+      res.send(500).send(e);
     });
 });
