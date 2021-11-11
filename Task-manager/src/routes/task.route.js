046375501915d6b3fac1905294cfd5dce736c1bc
S A HARIPRASAD <myharicode@gmail.com>
2021-11-11 22:55:37 +0530

    adding middleware for password encryption

--- Task-manager/src/routes/task.route.js
+++ Task-manager/src/routes/task.route.js
@@ -52,10 +52,9 @@ router.patch("/tasks/:id", async (req, res) => {
     return res.status(400).send({ error: "Invalid updates" });
   }
   try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    const task = await Task.findById(req.params.id);
+    allowedUpdates.forEach((update) => (task[update] = req.body[update]));
+    await task.save();
     if (!task) {
       return res.status(404).send();
     }
